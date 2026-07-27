dd614b7eb4e2ff35849869574fba895d76252222
Kazya Takahashi <95883403+QBeeIII@users.noreply.github.com>
2026-07-27 14:12:17 -0400

    foundation

--- BankAppRunner.java
+++ BankAppRunner.java
@@ -2,19 +2,56 @@ import java.util.*;
 
 public class BankAppRunner
 {
-    Scanner input = new Scanner(System.in);
-    public static void main(String[] args)
-    {
-        Bank b1 = new Bank(1, "ABC Digital Bank");
+    static Scanner sc = new Scanner(System.in);
 
-        System.err.println(b1.getName());
+    static Map<String, String> map = new HashMap<>();
 
-        
+    //seeding "database"
+    static {
+        map.put("admin","admin123");
+        map.put("rohit","rohit123");
+        map.put("mohit","mohit123");
+        map.put("shobhit","shobhit123");
     }
 
 
-}
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to our bank");
+        String loggedInUsername = mylogin();
+        //authenticateUser(loggedInUsername);
 
+        
+
+
+    }
+
+    private static String mylogin()
+    {
+        //keep retrying login until successful or force exit
+        while (true)
+        {
+            System.out.println("Please enter username and password separated by space");
+            String usernamePassword = sc.nextLine();
+            //validation
+            String[] tokens = usernamePassword.split(" ");
+            String enteredUsername = tokens[0];
+            String enteredPassword = tokens[1];
+
+            for (Map.Entry<String, String> me:map.entrySet())
+            {
+                String username = me.getKey();
+                String password = me.getValue();
+                if (username.equals(enteredUsername) && password.equals(enteredPassword))
+                {
+                    return username;
+                }
+            }
+
+            System.out.println("An error has occurred, please try again.");
+        }
+    }
+}
 
 
 class Bank
@@ -22,32 +59,92 @@ class Bank
     private int id;
     private String name;
 
-    public Bank(int ID, String NAME)
+    public Bank(int id, String name)
     {
-        id = ID;
-        name = NAME;
+        this.id = id;
+        this.name = name;
     }
 
     public Bank(){}
-
-
-    public void setId(int ID)
-    {
-        id = ID;
-    }
 
     public int getId()
     {
         return id;
     }
 
-    public void setName(String NAME)
+    public void setId(int ID)
     {
-        name = NAME;
+        id = ID;
     }
 
     public String getName()
     {
         return name;
     }
+
+    public void setName(String NAME)
+    {
+        name = NAME;
+    }
 }
+
+
+abstract class User
+{
+    protected String username;
+    protected String password;
+    protected boolean isAdmin;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String USERNAME)
+    {
+        username = USERNAME;
+    }
+
+    public String getPassword()
+    {
+        return username;
+    }
+
+    public void setPassword(String PASSWORD)
+    {
+        password = PASSWORD;
+    }
+
+    public boolean getAdminStatus()
+    {
+        return isAdmin;
+    }
+}
+
+class Admin extends User
+{
+    public Admin(String USERNAME, String PASSWORD)
+    {
+        username = USERNAME;
+        password = PASSWORD;
+        isAdmin = true;
+    }
+}
+
+class Customer extends User
+{
+    public Customer(String USERNAME, String PASSWORD)
+    {
+        username = USERNAME;
+        password = PASSWORD;
+        isAdmin = false;
+    }
+}
+
+
+//Abstract Class Account
+//CheckingsAccount extends Account
+//SavingsAccount extends Account
+
+//Interface AccountOperations: printInterestRate(), deposit, withdraw, transfer
+//SavingsAccount always gives higher interest rate
