7e3ef878b9c2bbb97c434c4cddae90d7a34d84ee
Kazya Takahashi <95883403+QBeeIII@users.noreply.github.com>
2026-07-28 10:46:34 -0400

    all except controller

---
+++ src/routes/customerRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const customerController = require('../controllers/customerController');
+
+const router = express.Router();
+
+router.get('/', customerController.getAll);
+router.get('/:id', customerController.getById);
+router.post('/', customerController.create);
+router.put('/:id', customerController.update);
+router.delete('/:id', customerController.delete);
+
+module.exports = router;
